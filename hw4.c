d6baa7770583edd79c2e2ce694ee9d46773f5987
MattGrider <mgride3@uic.edu>
2016-10-28 02:39:54 -0500

    trying to fix merge

--- hw4.c
+++ hw4.c
@@ -101,16 +101,104 @@ void build_heap_index() {
 
 // the actual collection code
 void sweep() {
-  // TODO
+  // 
+  size_t * walker = heap_mem.start;
+  walker = (walker - 1);
+  walker = next_chunk(walker);
+  while(walker < heap_mem.end)
+  {
+   // printf("Walker : %p Start : %p  End: %p\n", walker, heap_mem.start, heap_mem.end);
+    if(is_marked(walker))
+    {
+      clear_mark(walker);
+      walker = next_chunk(walker);
+    }
+    else if(in_use(walker))
+    {
+      //void * i = ((void *)walker) + 2;
+      //for(; i < (((void *)walker) +  chunk_size(walker)); i++)
+      //{
+      //  free(i);
+      //}
+      size_t * pointer_to_free = walker;
+      walker = next_chunk(walker);
+
+         free(pointer_to_free + 1);
+        // printf("free : %p\n", (pointer_to_free + 1));
+      
+      heap_mem.end=sbrk(0);
+    }
+    else
+    {
+      walker = next_chunk(walker);
+    }
+  }
+  
 }
 
 //determine if what "looks" like a pointer actually points to a block in the heap
 size_t * is_pointer(size_t * ptr) {
-  // TODO
+  //
+  if(ptr >= (next_chunk(heap_mem.start - 1)) && ptr < heap_mem.end)
+  {
+    size_t * walker = next_chunk(heap_mem.start - 1);
+    size_t * previous = walker;
+    walker = next_chunk(previous);
+    while(walker < heap_mem.end && walker != NULL)
+    {
+      if(previous <= ptr && walker > ptr)
+      {
+        if(in_use(previous))
+        {
+         //printf("Found!\n");
+          return previous;
+        }
+        else
+        {
+          return NULL;
+        }
+      }
+      previous = walker;
+      //printf("Walker : %p Pervious : %p Ptr : %p\n", walker, previous, ptr);
+      walker = next_chunk(walker);
+    }
+   // printf("NULL\n");
+    return NULL;
+  }
+  else
+  {
+    return NULL;
+  }
+}
+
+void mark_in_heap(size_t * p)
+{
+  size_t * b = is_pointer(p);
+  if(b == NULL) return;
+  if(is_marked(b)) return;
+  printf("markings nested : %p\n", p);
+  mark(b);
+  int i;
+  for(i = 0; i < chunk_size(b); i++)
+  {
+    mark_in_heap((b + i));
+  }
+  return;
 }
 
 void walk_region_and_mark(void* start, void* end) {
-  // TODO
+  if(start >= end) return;
+  size_t * b = is_pointer(start);
+  if(b == NULL) return walk_region_and_mark((start + 1), end);
+  if(is_marked(b)) return walk_region_and_mark((start + 1), end);
+  mark(b);
+  int i;
+  for(i = 0; i < chunk_size(b); i++)
+  {
+    mark_in_heap((b + i));
+  }
+  
+  return walk_region_and_mark((start + 1), end);
 }
 
 // standard initialization 
@@ -133,7 +221,7 @@ void gc() {
   // implementing this smart function for collecting garbage can get bonus;
   // if you can't figure it out, just comment out this function.
   // walk_region_and_mark and sweep are enough for this project.
-  build_heap_index();
+ // build_heap_index();
 
   //walk memory regions
   walk_region_and_mark(global_mem.start,global_mem.end);
